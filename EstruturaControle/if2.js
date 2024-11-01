f9977ac5476bf649643734c43c7440e225291d46
Jean <jeancarlos_bh@outlook.com>
2024-11-01 09:49:39 -0300

    if parte 3

--- EstruturaControle/if2.js
+++ EstruturaControle/if2.js
@@ -1,14 +1,14 @@
 function teste1(num){
     if(num >7)
         console.log(num)
-        console.log('Final') //Sempre sera executado
+        //console.log('Final') Sempre sera executado
 } 
 
 teste1(6)
 teste1(8)
 
 function teste2(num){
-    if(num >7); //Nao faz nada, esta sendo separado por ; do restante do codigo
+    if(num >7) //Nao faz nada, se estiver sendo separado por ; do restante do codigo
     {
         console.log(num)
     }
